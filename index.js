27d84adf539fd50ff74d5f7e767fab9399153c1b
yangchaojun <yangchaojunxs@126.com>
2020-02-13 11:17:11 +0800

    RESTful API最佳实践

--- index.js
+++ index.js
@@ -4,28 +4,37 @@ const Router = require('koa-router')
 const router = new Router()
 const usersRouter = new Router({prefix: '/users'})
 
-// 伪造鉴权
-const auth = async (ctx, next) => {
-  if (ctx.url !== '/users') {
-    ctx.throw(401)
-  }
-  await next()
-}
-
-router.get('/', auth, (ctx) => {
+router.get('/', (ctx) => {
   ctx.body = 'This is homepage'
 })
 
-usersRouter.get('/', auth, (ctx => {
-  ctx.body = 'This is users list page'
-}))
-
-usersRouter.post('/', auth, (ctx) => {
-  ctx.body = 'Create User'
+usersRouter.get('/', (ctx) => {
+  ctx.body = [
+    {name: '李雷'},
+    {name: '韩梅梅'}
+  ]
 })
 
-usersRouter.get('/:id', auth, (ctx) => {
-  ctx.body = `This is ${ctx.params.id}`
+usersRouter.post('/', (ctx) => {
+  ctx.body = {
+    name: '李雷'
+  }
+})
+
+usersRouter.get('/:id', (ctx) => {
+  ctx.body = {
+    name: '李雷'
+  }
+})
+
+usersRouter.put('/:id', (ctx) => {
+  ctx.body = {
+    name: '李雷2'
+  }
+})
+
+usersRouter.delete('/:id', (ctx) => {
+  ctx.status = 204
 })
 
 app.use(router.routes())
