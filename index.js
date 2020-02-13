974a4bd0d0f35b79e7f6fcbbc021f4da94e8e631
yangchaojun <yangchaojunxs@126.com>
2020-02-13 10:51:31 +0800

    koa-router用法：基础用法、前缀、多中间件

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const Koa = require('koa')
 const app = new Koa()
+const Router = require('koa-router')
+const router = new Router()
+const usersRouter = new Router({prefix: '/users'})
 
-app.use(async (ctx) => {
-  if (ctx.url === '/') {
-    ctx.body = '这是主页'
-  } else if (ctx.url === '/users') {
-    if (ctx.method === 'GET') {
-      ctx.body = 'This is users list page'
-    } else if (ctx.method === 'POST') {
-      ctx.body = 'Create User'
-    } else {
-      ctx.status = 405
-    }
-    
-  } else if (ctx.url.match(/\/users\/\w+/)) {
-    const userId = ctx.url.match(/\/users\/(\w+)/)[1]
-    ctx.body = `This is ${userId}`
-  } else {
-    ctx.status = 404
+// 伪造鉴权
+const auth = async (ctx, next) => {
+  if (ctx.url !== '/users') {
+    ctx.throw(401)
   }
- 
+  await next()
+}
+
+router.get('/', auth, (ctx) => {
+  ctx.body = 'This is homepage'
 })
+
+usersRouter.get('/', auth, (ctx => {
+  ctx.body = 'This is users list page'
+}))
+
+usersRouter.post('/', auth, (ctx) => {
+  ctx.body = 'Create User'
+})
+
+usersRouter.get('/:id', auth, (ctx) => {
+  ctx.body = `This is ${ctx.params.id}`
+})
+
+app.use(router.routes())
+app.use(usersRouter.routes())
 
 app.listen('8080')
