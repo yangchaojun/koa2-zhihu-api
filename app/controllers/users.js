5dd8b945d83ba71cbf2d9c5db1539b3c76e46cef
yangchaojun <yangchaojunxs@126.com>
2020-02-14 10:26:45 +0800

    引入koa-parameter校验参数

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,25 +5,45 @@ const db = [{
 class UsersCtl {
 
   getUsers(ctx) {
-    a.b
     ctx.body = db
   }
 
   setUser(ctx) {
+    ctx.verifyParams({
+      name: {
+        type: 'string',
+        required: true
+      }
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
 
   getSpecificUser(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
     ctx.body = db[ctx.params.id * 1]
   }
 
   updateSpecificUser(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
+    ctx.verifyParams({
+      name: {
+        type: 'string',
+        required: true
+      }
+    })
     db[ctx.params.id * 1] = ctx.request.body
     ctx.body = ctx.request.body
   }
 
   deleteSpecificUser(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
     db.splice(ctx.params.id, 1)
     ctx.status = 204
   }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const app = new Koa()
 
 const routing = require('./routes')
@@ -9,6 +10,7 @@ app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
 }))
 app.use(bodyParser())
+app.use(parameter(app))
 
 // 注册路由
 routing(app)
